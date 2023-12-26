605f35cbaa844c4b2da6859f8e245582bab74ada
Shubhanshu <32585229+Shubhansh-Simple@users.noreply.github.com>
2023-12-26 23:21:31 +0530

    Update models.py

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,5 @@
-from django.db   import  models
-from django.contrib.auth  import get_user_model
-from django.conf import  settings
+from django.db           import  models
+from django.conf         import  settings
 
 
 class Projects( models.Model ):
@@ -9,27 +8,33 @@ class Projects( models.Model ):
         verbose_name_plural = 'My-Projects'
 
     creator         = models.ForeignKey( settings.AUTH_USER_MODEL,on_delete=models.CASCADE )
-
-    title           = models.CharField( max_length=100 ,help_text='Title of the projects')
-    description     = models.CharField( max_length=600 ,help_text='Description of that projects')
+    title           = models.CharField( max_length=100 ,help_text='Title of the project')
+    description     = models.CharField( max_length=700 ,help_text='Description of that project')
+    
+    # Tools used for creating the project
+    tools           = models.CharField( max_length=700 ,
+                                        help_text='Tools used for creating this project (Separate tool by comma and not more than 5 tools)',
+                                        null=True, blank=True )
 
     # images
-    isometric_image = models.ImageField( upload_to='images/', help_text='Upload isometric view of the model.')
-    front_image     = models.ImageField( upload_to='images/', help_text='Upload front view of the model.')
-    side_image      = models.ImageField( upload_to='images/', help_text='Upload side view of the model.')
-    top_image       = models.ImageField( upload_to='images/', help_text='(OPTIONAL) Upload top view of the model.', null=True,blank=True )
-    bottom_image    = models.ImageField( upload_to='images/', help_text='(OPTIONAL) Upload bottom view of the model.', null=True,blank=True )
-
-    # title of projects images in detail page
+    isometric_image       = models.ImageField( upload_to='images/', help_text='Upload isometric view of the model.')
     isometric_image_title = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )
-    front_image_title     = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )    
-    side_image_title      = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )     
-    top_image_title       = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )      
-    bottom_image_title    = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )       
+
+    front_image        = models.ImageField( upload_to='images/', help_text='Upload front view of the model.')
+    front_image_title  = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )
+
+    side_image         = models.ImageField( upload_to='images/', help_text='(OPTIONAL) Upload side view of the model.',null=True, blank=True )
+    side_image_title   = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )
+
+    top_image          = models.ImageField( upload_to='images/', help_text='(OPTIONAL) Upload top view of the model.', null=True,blank=True )
+    top_image_title    = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )
+
+    bottom_image       = models.ImageField( upload_to='images/', help_text='(OPTIONAL) Upload bottom view of the model.', null=True,blank=True )
+    bottom_image_title = models.CharField( max_length=50, help_text='(OPTIONAL) Title of projects images', null=True, blank=True )
 
     project_file          = models.FileField( upload_to ='project_file/', help_text='(OPTIONAL) Upload Your Project file.', null=True, blank=True )
     project_file_link     = models.URLField( max_length=100, null=True, blank=True, help_text='(OPTIONAL) For eg. github link' )
-        
+
     def __str__( self ):
         return str( self.title ).capitalize()
 
